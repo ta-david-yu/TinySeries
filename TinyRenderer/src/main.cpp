d45b42d80efe92cb197f00da61a71b3428b12d61
Ta David Yu <ta.yu.gamedev@gmail.com>
2024-12-20 10:40:07 +0800

    Tiny renderer - lesson 1: third attempt

--- TinyRenderer/src/main.cpp
+++ TinyRenderer/src/main.cpp
@@ -3,6 +3,7 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
+// First attempt
 void inefficientConstantStepLine(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor& color)
 {
     const float step = 0.01f;
@@ -14,6 +15,7 @@ void inefficientConstantStepLine(int x0, int y0, int x1, int y1, TGAImage &image
     }
 }
 
+// Second attempt
 void widthBasedStepLine(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor& color)
 {
     for (int x = x0; x <= x1; x++)
@@ -24,6 +26,42 @@ void widthBasedStepLine(int x0, int y0, int x1, int y1, TGAImage &image, const T
     }
 }
 
+// Third attempt
+void widthBasedStepLineFixingAliasingAndLoopOrder(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor& color)
+{
+
+    const bool isLineTooSteep = std::abs(x0 - x1) < std::abs(y0 - y1);
+    const bool isXYSwapped = isLineTooSteep;
+    if (isLineTooSteep)
+    {
+        // If the line is too steep, we loop through the y-axis instead of the x.
+        std::swap(x0, y0);
+        std::swap(x1, y1);
+    }
+
+    if (x0 > x1)
+    {
+        // We want to make sure the loop goes from left to right.
+        std::swap(x0, x1);
+        std::swap(y0, y1);
+    }
+
+    for (int x = x0; x <= x1; x++)
+    {
+        const float t = (x - x0) / (float)(x1 - x0);
+        const int y = y0 * (1.0f - t) + y1 * t;
+
+        if (isXYSwapped)
+        {
+            image.set(y, x, color);
+        }
+        else
+        {
+            image.set(x, y, color);
+        }
+    }
+}
+
 int main(int argc, char** argv) {
     TGAImage image(100, 100, TGAImage::RGB);
 
@@ -31,13 +69,23 @@ int main(int argc, char** argv) {
     /*
     {
         inefficientConstantStepLine(13, 20, 80, 40, image, white);
-    }*/
+    }
+    */
 
     // Second attempt
+    /*
     {
         widthBasedStepLine(13, 20, 80, 40, image, white);
         widthBasedStepLine(20, 13, 40, 80, image, red);
         widthBasedStepLine(80, 40, 13, 20, image, red);
+    }
+    */
+
+    // Third attempt
+    {
+        widthBasedStepLineFixingAliasingAndLoopOrder(13, 20, 80, 40, image, white);
+        widthBasedStepLineFixingAliasingAndLoopOrder(20, 13, 40, 80, image, red);
+        widthBasedStepLineFixingAliasingAndLoopOrder(80, 40, 13, 20, image, red);
     }
 
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
