2c863e944d9340997fc5e98906b0de0dd3dc5634
Ta David Yu <ta.yu.gamedev@gmail.com>
2024-12-20 10:28:14 +0800

    Tiny renderer - lesson 1: second attempt

--- TinyRenderer/src/main.cpp
+++ TinyRenderer/src/main.cpp
@@ -3,7 +3,7 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
-void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor& color)
+void inefficientConstantStepLine(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor& color)
 {
     const float step = 0.01f;
     for (float t = 0; t < 1.0f; t += step)
@@ -14,9 +14,32 @@ void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor& color
     }
 }
 
+void widthBasedStepLine(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor& color)
+{
+    for (int x = x0; x <= x1; x++)
+    {
+        const float t = (x - x0) / (float)(x1 - x0);
+        const int y = y0 * (1.0f - t) + y1 * t;
+        image.set(x, y, color);
+    }
+}
+
 int main(int argc, char** argv) {
     TGAImage image(100, 100, TGAImage::RGB);
-    line(13, 20, 80, 40, image, white);
+
+    // First attempt
+    /*
+    {
+        inefficientConstantStepLine(13, 20, 80, 40, image, white);
+    }*/
+
+    // Second attempt
+    {
+        widthBasedStepLine(13, 20, 80, 40, image, white);
+        widthBasedStepLine(20, 13, 40, 80, image, red);
+        widthBasedStepLine(80, 40, 13, 20, image, red);
+    }
+
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     return 0;
