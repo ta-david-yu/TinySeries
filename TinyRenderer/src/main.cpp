8d8cbefa6b364eb7e9cad1519e643053daaf72de
Ta David Yu <ta.yu.gamedev@gmail.com>
2024-12-20 10:16:48 +0800

    Tiny renderer - lesson 1: first attempt

--- TinyRenderer/src/main.cpp
+++ TinyRenderer/src/main.cpp
@@ -3,9 +3,20 @@
 const TGAColor white = TGAColor(255, 255, 255, 255);
 const TGAColor red   = TGAColor(255, 0,   0,   255);
 
+void line(int x0, int y0, int x1, int y1, TGAImage &image, const TGAColor& color)
+{
+    const float step = 0.01f;
+    for (float t = 0; t < 1.0f; t += step)
+    {
+        const int x = x0 + (x1 - x0) * t;
+        const int y = y0 + (y1 - y0) * t;
+        image.set(x, y, color);
+    }
+}
+
 int main(int argc, char** argv) {
     TGAImage image(100, 100, TGAImage::RGB);
-    image.set(52, 41, red);
+    line(13, 20, 80, 40, image, white);
     image.flip_vertically(); // i want to have the origin at the left bottom corner of the image
     image.write_tga_file("output.tga");
     return 0;
